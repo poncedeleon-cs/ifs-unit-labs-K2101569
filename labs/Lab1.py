4b80d465cd19402a2f3665408e221b1d7f0e7664
K2101569 <K2101569@students.katyisd.org>
2024-10-10 17:40:32 +0000

    Finished Lab 1

--- labs/Lab1.py
+++ labs/Lab1.py
@@ -1,7 +1,28 @@
-# Name:
-# Period:
-# Date:
+# Name: Ericka Tamayo
+# Period: 5th / Mr. P
+# Date: 10.10.24
     
 # define a function isLeapYear() that takes a given year as
 # a parameter and returns true if the given year is a leap
 # year and false if the given year is not a leap year.
+
+def leapYear(year):
+    # Check if the year is divisible by 400, or divisible by 4 but not by 100
+    if (year % 400 == 0):
+        return True
+    elif year % 4 == 0 and year % 100 != 0:
+        return True
+    else:
+        return False
+
+print(leapYear(1804))
+print(leapYear(99))
+print(leapYear(400))
+print(leapYear(4000))
+print(leapYear(4004))
+print(leapYear(2200))
+print(leapYear(1900))
+print(leapYear(1996))
+print(leapYear(1924))
+print(leapYear(2112))
+print(leapYear(1997))
